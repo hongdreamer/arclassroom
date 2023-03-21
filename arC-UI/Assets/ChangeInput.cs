11b4b7acca9389fbb1d8f10c09d0e27f16c6a754
cxw0520 <james30088@gmail.com>
2023-03-22 00:14:24 +0800

    UI upload
    UI第一次上傳 by cxw

---
+++ arC-UI/Assets/ChangeInput.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ChangeInput : MonoBehaviour
+{
+    EventSystem system;
+    public Selectable firstInput;
+    public Button submitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = EventSystem.current;
+        firstInput.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift))
+        {
+            Selectable previos = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+            if (previos != null)
+            {
+                previos.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            if (next != null)
+            {
+                next.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            submitButton.onClick.Invoke();
+            Debug.Log("Button Pressed");
+        }
+        
+    }
+}
